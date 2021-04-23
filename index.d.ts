9cc238bf3018220fb003f75e24c41854ae0db676
y-dojo <y.dojo.sysdj@gmail.com>
2021-04-23 17:35:22 +0900

    update d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,13 @@
-/**
- * ydj module definition
- */
+export = ydj;
+
 export as namespace ydj;
+
+declare namespace ydj {
+  class IStore<T> {
+    state?: T;
+    init(): T;
+    actions: {
+      [action: string]: (...args: any[]) => T | void;
+    };
+  }
+}
